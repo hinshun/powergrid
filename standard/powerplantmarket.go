e112f03162563ec4ed01c0e93fc6d581296125c5
Edgar Lee <edgar.lee@docker.com>
2017-01-26 22:28:24 -0800

    Implement standard powerplant market
    Signed-off-by: Edgar Lee <edgar.lee@docker.com>

---
+++ standard/powerplantmarket.go
@@ -0,0 +1,54 @@
+package standard
+
+import (
+	"errors"
+
+	"github.com/hinshun/powergrid"
+)
+
+var (
+	ErrPowerPlantNotFound = errors.New("power plant not found")
+)
+
+type powerPlantMarket struct {
+	powerPlants []powergrid.PowerPlant
+}
+
+func NewPowerPlantMarket(powerPlants []powergrid.PowerPlant) powergrid.PowerPlantMarket {
+	market := &powerPlantMarket{powerPlants: powerPlants}
+
+	for _, powerPlant := range powerPlants {
+		market.Add(powerPlant)
+	}
+
+	return market
+}
+
+func (ppm *powerPlantMarket) Inventory() []powergrid.PowerPlant {
+	return ppm.powerPlants
+}
+
+func (ppm *powerPlantMarket) Add(addPlant powergrid.PowerPlant) {
+	for i, powerPlant := range ppm.powerPlants {
+		if addPlant.Ordinal() < powerPlant.Ordinal() {
+			ppm.powerPlants = append(
+				append(ppm.powerPlants[:i], addPlant),
+				ppm.powerPlants[i+1:],
+			)
+			return
+		}
+	}
+
+	ppm.powerPlants = append(ppm.powerPlants, addPlant)
+}
+
+func (ppm *powerPlantMarket) Remove(removePlant powergrid.PowerPlant) error {
+	for i, powerPlant := range ppm.powerPlants {
+		if removePlant == powerPlant {
+			ppm.powerPlants = append(ppm.powerPlants[:i], ppm.powerPlants[i+1:])
+			return nil
+		}
+	}
+
+	return ErrPowerPlantNotFound
+}
